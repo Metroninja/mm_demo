95c8d0ee631981dbf93693c0a4048dfaaad0d2af
Erik Steinfeld <eriks@attackpattern.com>
2018-06-09 12:51:23 -0700

    Home page rendering all albums

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ActivityIndicator, Image, StyleSheet,
+import { ActivityIndicator, FlatList, StyleSheet,
   Text, TouchableOpacity, View } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 import globalStyle from '../../styles';
 
@@ -29,8 +30,12 @@ export default class Home extends Component {
     this.state = {
     }
   }
-  renderLoading() {
-    return (
+
+  renderHeader(albumsLoaded) {
+    
+    return albumsLoaded ? ( 
+      <Text style={[styles.title, styles.header]}>User Albums</Text>
+      ) : (
       <View style={[styles.row, styles.loading]}>
         <Text>Loading Albums</Text>
         <ActivityIndicator />
@@ -38,22 +43,53 @@ export default class Home extends Component {
     )
   }
 
+  renderAlbum(album) {
+    return (
+      <TouchableOpacity style={[styles.album, album.index < this.props.albums.length - 1 && styles.seperator]}>
+        <Text style={styles.title}>{`User ${album.item.userId}`}</Text>
+        <Text style={styles.text}>{album.item.title}</Text>
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     const { albums } = this.props;
-    console.log('albums', albums)
     return (
-      <View>
-        {albums.length === 0 && this.renderLoading()}
-      </View>
+      <KeyboardAwareScrollView style={styles.container}>
+        {this.renderHeader(!!albums.length)}
+        {!!albums.length && (
+          <FlatList
+          data={albums}
+          renderItem={album => this.renderAlbum(album)}
+          keyExtractor={(album, index) => `album-id-${album.id}`}
+        />
+        )}
+      </KeyboardAwareScrollView>
     )
   }
 }
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   row: {
     ...globalStyle.row,
     justifyContent: 'center'
   },
+  title: globalStyle.title,
+  text: globalStyle.text,
+  header: {
+    marginTop: 12,
+    textAlign: 'center'
+  },
   loading: {
     marginTop: 12
+  },
+  album: {
+    padding: 12,
+  },
+  seperator: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: globalStyle.lightGray
   }
 });
